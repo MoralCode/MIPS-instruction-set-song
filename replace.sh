877c5341ad7e8abc81f3c7530a5120686db0eaa7
Adrian Edwards <17362949+MoralCode@users.noreply.github.com>
2021-12-27 19:16:54 -0800

    correct the replacement of short segments

--- replace.sh
+++ replace.sh
@@ -8,14 +8,14 @@ sed -i -e 's/sll/sill/g' $1
 sed -i -e 's/bgezal/beh-geezal/g' $1
 sed -i -e 's/multu/mult-you/g' $1
 sed -i -e 's/srav/srav (one syllable)/g' $1
-sed -i -e 's/j/jay/g' $1
+sed -i -e 's/ j / jay /g' $1
 sed -i -e 's/lhu/l-h-u/g' $1
 sed -i -e 's/lui/l-u-i/g' $1
 sed -i -e 's/slt/slit/g' $1
 sed -i -e 's/bne/b-n-e/g' $1
 sed -i -e 's/mthi/m-t-high/g' $1
-sed -i -e 's/sh/shih/g' $1
-sed -i -e 's/sb/sib/g' $1
+sed -i -e 's/ sh / shih /g' $1
+sed -i -e 's/ sb / sib /g' $1
 sed -i -e 's/lbu/l-b-u/g' $1
 sed -i -e 's/blez/blez/g' $1
 sed -i -e 's/sltu/slit-u/g' $1
@@ -28,10 +28,10 @@ sed -i -e 's/bgez/beh-gez/g' $1
 sed -i -e 's/jalr/jal-er/g' $1
 sed -i -e 's/addi/addy/g' $1
 sed -i -e 's/divu/div-u/g' $1
-sed -i -e 's/lh/l-h/g' $1
+sed -i -e 's/ lh / l-h /g' $1
 sed -i -e 's/rfe/reef/g' $1
 sed -i -e 's/syscall/sis-call/g' $1
-sed -i -e 's/jr/junior/g' $1
+sed -i -e 's/ jr / junior /g' $1
 sed -i -e 's/mfcz/miff-c-z/g' $1
 sed -i -e 's/mfc1/m-f-c-one/g' $1
 sed -i -e 's/nop/nopp/g' $1
@@ -40,7 +40,7 @@ sed -i -e 's/bczt/b-c-z-t/g' $1
 sed -i -e 's/srlv/s-r-l-v/g' $1
 sed -i -e 's/xori/zorri/g' $1
 sed -i -e 's/bltz/blitz/g' $1
-sed -i -e 's/lb/l-b/g' $1
+sed -i -e 's/ lb / l-b /g' $1
 sed -i -e 's/lwl/lwill/g' $1
 sed -i -e 's/addiu/addy-you/g' $1
 sed -i -e 's/andi/andy/g' $1
@@ -52,7 +52,7 @@ sed -i -e 's/sltiu/slitty-u/g' $1
 sed -i -e 's/bzcf/b-z-c-f/g' $1
 sed -i -e 's/bgtz/b-g-t-z/g' $1
 sed -i -e 's/mflo/em-flow/g' $1
-sed -i -e 's/sw/swee/g' $1
+sed -i -e 's/ sw / swee /g' $1
 sed -i -e 's/mtlo/m-t-low/g' $1
 sed -i -e 's/swcz/swizzy/g' $1
-sed -i -e 's/lw/lwee/g' $1
+sed -i -e 's/ lw / lwee /g' $1
